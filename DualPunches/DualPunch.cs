d3c4b7e7c9ef887da7ba04544fd2055e42645501
eternalUnion <dnzbyk12345@gmail.com>
2023-05-07 17:35:00 +0300

    Add project files.

---
+++ DualPunches/DualPunch.cs
@@ -0,0 +1,443 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace DualPunches
+{
+    public class HookArmCloneComp : MonoBehaviour
+    {
+        public bool attemptedToThrow = false;
+    }
+
+    public class DualPunch : MonoBehaviour
+    {
+        public float delay = 0f;
+        public float juiceAmount = 0f;
+        private bool juiceGiven = false;
+
+        private GameObject currentPunch = null;
+        private Punch currentComp = null;
+        private GameObject currentHookarm;
+        private HookArm currentHookarmComp;
+        private HookArmCloneComp currentHookarmFlag;
+        private Animator anim;
+        private GameObject copyTarget;
+        private PowerUpMeter meter;
+        private FistControl fc;
+
+        private HookArm originalInstance;
+
+        static PropertyInfo hookArmInstance = typeof(MonoSingleton<HookArm>).GetProperty("Instance", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+
+        private void Start()
+        {
+            this.fc = MonoSingleton<FistControl>.Instance;
+            copyTarget = fc.currentArmObject;
+            this.meter = MonoSingleton<PowerUpMeter>.Instance;
+            if (this.juiceAmount == 0f)
+            {
+                this.juiceAmount = 30f;
+            }
+            if (this.meter.juice < this.juiceAmount)
+            {
+                this.meter.latestMaxJuice = this.juiceAmount;
+                this.meter.juice = this.juiceAmount;
+            }
+            this.meter.powerUpColor = new Color(1f, 0.6f, 0f);
+            this.juiceGiven = true;
+
+            if (this.fc.currentArmObject)
+            {
+                this.UpdatePunch();
+            }
+
+            originalInstance = HookArm.Instance;
+            currentHookarm = GameObject.Instantiate(Plugin.goldenArm/*fc.gameObject.GetComponentInChildren<HookArm>().gameObject*/, transform);
+            currentHookarm.SetActive(true);
+            hookArmInstance.SetValue(null, originalInstance);
+
+            //currentHookarm.transform.localRotation = Quaternion.identity;
+            currentHookarmComp = currentHookarm.GetComponent<HookArm>();
+            currentHookarmFlag = currentHookarmComp.gameObject.AddComponent<HookArmCloneComp>();
+            //currentHookarmComp.enabled = false;
+            //currentHookarmComp.SendMessage("Start");
+        }
+
+        private void UpdatePunch()
+        {
+            if (currentPunch != null)
+                Destroy(currentPunch);
+
+            copyTarget = fc.currentArmObject;
+            if (copyTarget == null)
+                return;
+
+            if(copyTarget.gameObject.name.StartsWith("Arm Blue"))
+                currentPunch = Instantiate(Plugin.blueArm, transform);
+            else if(copyTarget.gameObject.name.StartsWith("Arm Red"))
+                currentPunch = Instantiate(Plugin.redArm, transform);
+            else
+                currentPunch = Instantiate(copyTarget, transform);
+
+            Punch comp = currentComp = currentPunch.GetComponent<Punch>();
+            comp.Invoke("Start", 0f);
+            comp.enabled = false;
+            anim = currentPunch.GetComponent<Animator>();
+        }
+
+        bool createdArm = false;
+
+        static FieldInfo hookarmCooldown = typeof(HookArm).GetField("cooldown", BindingFlags.NonPublic | BindingFlags.Instance);
+        private bool readyToPunch = false;
+        private void Update()
+        {
+            if(HookArm.Instance != originalInstance)
+                hookArmInstance.SetValue(null, originalInstance);
+
+            if (fc.fistCooldown <= 0)
+                readyToPunch = true;
+
+            if (!createdArm)
+            {
+                UpdatePunch();
+                createdArm = true;
+            }
+
+            if (this.juiceGiven && this.meter.juice <= 0f)
+            {
+                this.EndPowerUp();
+                return;
+            }
+            if (!this.copyTarget || this.copyTarget != this.fc.currentArmObject)
+            {
+                this.UpdatePunch();
+            }
+            if (this.currentPunch)
+            {
+                if (!this.fc.currentArmObject.activeInHierarchy && this.currentPunch.activeSelf)
+                {
+                    this.currentPunch.SetActive(false);
+                    return;
+                }
+                if (this.fc.currentArmObject.activeInHierarchy && !this.currentPunch.activeSelf)
+                {
+                    this.currentPunch.SetActive(true);
+                }
+            }
+
+            if(this.currentPunch != null)
+            {
+                if (MonoSingleton<InputManager>.Instance.InputSource.Hook.IsPressed)
+                {
+                    currentComp.CancelInvoke("PunchStart");
+                    currentComp.SendMessage("CancelAttack");
+                    if(MonoSingleton<InputManager>.Instance.InputSource.Hook.WasPerformedThisFrame)
+                    {
+                        if (currentHookarmComp.state == HookState.Ready)
+                            currentHookarmFlag.attemptedToThrow = true;
+                    }
+                }
+                else
+                {
+                    if (currentHookarmComp.state == HookState.Ready)
+                    {
+                        hookarmCooldown.SetValue(currentHookarmComp, delay);
+                        currentHookarmFlag.attemptedToThrow = false;
+                    }
+
+                    bool punchButton = MonoSingleton<InputManager>.Instance.InputSource.Punch.WasPerformedThisFrame;
+                    if(Plugin.fastPunchEnabled)
+                    {
+                        if (currentPunch.name.StartsWith("Arm Red"))
+                            punchButton = MonoSingleton<InputManager>.Instance.InputSource.ChangeFist.WasPerformedThisFrame;
+                    }
+
+                    if (punchButton && currentComp.ready && (this.fc.fistCooldown <= 0f || readyToPunch) && this.fc.activated && !GameStateManager.Instance.PlayerInputLocked)
+                    {
+                        currentComp.Invoke("PunchStart", delay);
+                        currentHookarmComp.Invoke("Cancel", delay);
+                        readyToPunch = false;
+                        //currentHookarmComp.SendMessage("Cancel");
+                    }
+                }
+            }
+        }
+
+        static FieldInfo hookarmCurrentEid = typeof(HookArm).GetField("caughtEid", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        static FieldInfo hookarmEnemyGroundCheck = typeof(HookArm).GetField("enemyGroundCheck", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        public void EndPowerUp()
+        {
+            if(currentHookarm != null)
+            {
+                EnemyIdentifier eid = (EnemyIdentifier)hookarmCurrentEid.GetValue(currentHookarmComp);
+                if(eid != null)
+                {
+                    eid.hooked = false;
+                    GroundCheckEnemy gce = (GroundCheckEnemy)hookarmEnemyGroundCheck.GetValue(currentHookarmComp);
+                    if (gce != null)
+                        gce.StopForceOff();
+                    if (eid.gce != null)
+                        eid.gce.ForceOff();
+                }
+            }
+
+            Destroy(base.gameObject);
+        }
+    }
+
+    [HarmonyPatch(typeof(HookArm), "Update")]
+    class HookArm_Update
+    {
+        // This must be ran as the lastest patch for possible
+        // compability with other plugins
+        [HarmonyAfter]
+        static bool Prefix(HookArm __instance, GameObject ___model, ref float ___cooldown, ref bool ___forcingFistControl,
+            LineRenderer ___lr, ref Vector3 ___caughtPoint, ref bool ___returning, ref Vector3 ___hookPoint,
+            ref Vector3 ___previousHookPoint, CameraFrustumTargeter ___targeter, ref Vector3 ___throwDirection,
+            List<Rigidbody> ___caughtObjects, ref bool ___lightTarget, Animator ___anim, LineRenderer ___inspectLr,
+            ref float ___throwWarp, AudioSource ___aud, ref float ___semiBlocked, ref Transform ___caughtTransform,
+            ref Collider ___caughtCollider, ref EnemyIdentifier ___caughtEid, float ___returnDistance)
+        {
+            if (!MonoSingleton<OptionsManager>.Instance || MonoSingleton<OptionsManager>.Instance.paused)
+            {
+                return false;
+            }
+            if (!HookArm.Instance.equipped || MonoSingleton<FistControl>.Instance.shopping || !MonoSingleton<FistControl>.Instance.activated)
+            {
+                if (__instance.state != HookState.Ready || ___returning)
+                {
+                    __instance.Cancel();
+                }
+                ___model.SetActive(false);
+                return false;
+            }
+            HookArmCloneComp comp = __instance.GetComponent<HookArmCloneComp>();
+            if (  ((comp == null && MonoSingleton<InputManager>.Instance.InputSource.Hook.WasPerformedThisFrame)
+                || (comp != null && comp.attemptedToThrow && MonoSingleton<InputManager>.Instance.InputSource.Hook.IsPressed)))//CHANGED
+            {
+                if (__instance.state == HookState.Pulling)
+                {
+                    __instance.StopThrow(0f, false);
+                }
+                else if (___cooldown <= 0f)
+                {
+                    if(comp)
+                        comp.attemptedToThrow = false;
+                    ___cooldown = 0.5f;
+                    ___model.SetActive(true);
+                    if (!___forcingFistControl)
+                    {
+                        if (MonoSingleton<FistControl>.Instance.currentPunch)
+                        {
+                            MonoSingleton<FistControl>.Instance.currentPunch.CancelAttack();
+                        }
+                        MonoSingleton<FistControl>.Instance.forceNoHold++;
+                        ___forcingFistControl = true;
+                        MonoSingleton<FistControl>.Instance.transform.localRotation = Quaternion.identity;
+                    }
+                    ___lr.enabled = true;
+                    ___hookPoint = __instance.transform.position;
+                    ___previousHookPoint = ___hookPoint;
+                    if (___targeter.CurrentTarget && ___targeter.IsAutoAimed)
+                    {
+                        ___throwDirection = (___targeter.CurrentTarget.bounds.center - __instance.transform.position).normalized;
+                    }
+                    else
+                    {
+                        ___throwDirection = __instance.transform.forward;
+                    }
+                    ___returning = false;
+                    if (___caughtObjects.Count > 0)
+                    {
+                        foreach (Rigidbody rigidbody in ___caughtObjects)
+                        {
+                            if (rigidbody)
+                            {
+                                rigidbody.velocity = (MonoSingleton<NewMovement>.Instance.transform.position - rigidbody.transform.position).normalized * (100f + ___returnDistance / 2f);
+                            }
+                        }
+                        ___caughtObjects.Clear();
+                    }
+                    __instance.state = HookState.Throwing;
+                    ___lightTarget = false;
+                    ___throwWarp = 1f;
+                    ___anim.Play("Throw", -1, 0f);
+                    ___inspectLr.enabled = false;
+                    __instance.hand.transform.localPosition = new Vector3(0.09f, -0.051f, 0.045f);
+                    if (MonoSingleton<CameraController>.Instance.defaultFov > 105f)
+                    {
+                        __instance.hand.transform.localPosition += new Vector3(0.225f * ((MonoSingleton<CameraController>.Instance.defaultFov - 105f) / 55f), -0.25f * ((MonoSingleton<CameraController>.Instance.defaultFov - 105f) / 55f), 0.05f * ((MonoSingleton<CameraController>.Instance.defaultFov - 105f) / 55f));
+                    }
+                    ___caughtPoint = Vector3.zero;
+                    ___caughtTransform = null;
+                    ___caughtCollider = null;
+                    ___caughtEid = null;
+                    GameObject.Instantiate<GameObject>(__instance.throwSound);
+                    ___aud.clip = __instance.throwLoop;
+                    ___aud.panStereo = 0f;
+                    ___aud.Play();
+                    ___aud.pitch = UnityEngine.Random.Range(0.9f, 1.1f);
+                    ___semiBlocked = 0f;
+                    MonoSingleton<RumbleManager>.Instance.SetVibrationTracked("rumble.whiplash.throw", __instance.gameObject);
+                }
+            }
+            if (___cooldown != 0f)
+            {
+                ___cooldown = Mathf.MoveTowards(___cooldown, 0f, Time.deltaTime);
+            }
+            if (___lr.enabled)
+            {
+                ___throwWarp = Mathf.MoveTowards(___throwWarp, 0f, Time.deltaTime * 6.5f);
+                ___lr.SetPosition(0, __instance.hand.position);
+                for (int i = 1; i < ___lr.positionCount - 1; i++)
+                {
+                    float d = 3f;
+                    if (i % 2 == 0)
+                    {
+                        d = -3f;
+                    }
+                    ___lr.SetPosition(i, Vector3.Lerp(__instance.hand.position, ___hookPoint, (float)i / (float)___lr.positionCount) + __instance.transform.up * d * ___throwWarp * (1f / (float)i));
+                }
+                ___lr.SetPosition(___lr.positionCount - 1, ___hookPoint);
+            }
+            if (__instance.state == HookState.Pulling && !___lightTarget && MonoSingleton<InputManager>.Instance.InputSource.Jump.WasPerformedThisFrame)
+            {
+                if (MonoSingleton<NewMovement>.Instance.rb.velocity.y < 1f)
+                {
+                    MonoSingleton<NewMovement>.Instance.rb.velocity = new Vector3(MonoSingleton<NewMovement>.Instance.rb.velocity.x, 1f, MonoSingleton<NewMovement>.Instance.rb.velocity.z);
+                }
+                MonoSingleton<NewMovement>.Instance.rb.velocity = Vector3.ClampMagnitude(MonoSingleton<NewMovement>.Instance.rb.velocity, 30f);
+                if (!MonoSingleton<NewMovement>.Instance.gc.touchingGround && !Physics.Raycast(MonoSingleton<NewMovement>.Instance.gc.transform.position, Vector3.down, 1.5f, LayerMaskDefaults.Get(LMD.EnvironmentAndBigEnemies)))
+                {
+                    MonoSingleton<NewMovement>.Instance.rb.AddForce(Vector3.up * 15f, ForceMode.VelocityChange);
+                }
+                else if (!MonoSingleton<NewMovement>.Instance.jumping)
+                {
+                    MonoSingleton<NewMovement>.Instance.Jump();
+                }
+                __instance.StopThrow(1f, false);
+            }
+            if (MonoSingleton<FistControl>.Instance.currentPunch && MonoSingleton<FistControl>.Instance.currentPunch.holding && ___forcingFistControl)
+            {
+                MonoSingleton<FistControl>.Instance.currentPunch.heldItem.transform.position = __instance.hook.position + __instance.hook.up * 0.2f;
+                if (__instance.state != HookState.Ready || ___returning)
+                {
+                    MonoSingleton<FistControl>.Instance.heldObject.hooked = true;
+                    if (MonoSingleton<FistControl>.Instance.heldObject.gameObject.layer != 22)
+                    {
+                        Transform[] componentsInChildren = MonoSingleton<FistControl>.Instance.heldObject.GetComponentsInChildren<Transform>();
+                        for (int j = 0; j < componentsInChildren.Length; j++)
+                        {
+                            componentsInChildren[j].gameObject.layer = 22;
+                        }
+                        return false;
+                    }
+                }
+                else
+                {
+                    MonoSingleton<FistControl>.Instance.heldObject.hooked = false;
+                    if (MonoSingleton<FistControl>.Instance.heldObject.gameObject.layer != 13)
+                    {
+                        Transform[] componentsInChildren = MonoSingleton<FistControl>.Instance.heldObject.GetComponentsInChildren<Transform>();
+                        for (int j = 0; j < componentsInChildren.Length; j++)
+                        {
+                            componentsInChildren[j].gameObject.layer = 13;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(HookArm), "FixedUpdate")]
+    class HookArm_FixedUpdate
+    {
+        static ItemIdentifier temporaryIdentifier;
+        public static bool cloneArmPlacingObject = false;
+
+        struct StateInfo
+        {
+            public bool cloneArm;
+            public bool holding;
+            public ItemIdentifier heldObject;
+
+            public StateInfo(bool holding, ItemIdentifier heldObject)
+            {
+                this.holding = holding;
+                this.heldObject = heldObject;
+                cloneArm = false;
+            }
+        }
+
+        static bool Prefix(HookArm __instance, out StateInfo __state)
+        {
+            __state = new StateInfo(MonoSingleton<FistControl>.Instance.currentPunch.holding, MonoSingleton<FistControl>.Instance.heldObject);
+            HookArmCloneComp flag = __instance.GetComponent<HookArmCloneComp>();
+            if (flag == null)
+                return true;
+            __state.cloneArm = true;
+            cloneArmPlacingObject = true;
+
+            /*if(temporaryIdentifier == null)
+            {
+                GameObject obj = new GameObject();
+                temporaryIdentifier = obj.AddComponent<ItemIdentifier>();
+                temporaryIdentifier.itemType = ItemType.None;
+            }
+
+            MonoSingleton<FistControl>.Instance.currentPunch.holding = true;
+            MonoSingleton<FistControl>.Instance.heldObject = temporaryIdentifier;*/
+            return true;
+        }
+
+        static void Postfix(HookArm __instance, StateInfo __state)
+        {
+            if (!__state.cloneArm)
+                return;
+            //MonoSingleton<FistControl>.Instance.currentPunch.holding = __state.holding;
+            //MonoSingleton<FistControl>.Instance.heldObject = __state.heldObject;
+            cloneArmPlacingObject = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Punch), "PlaceHeldObject")]
+    class Punch_PlaceHeldObject
+    {
+        static bool Prefix(Punch __instance)
+        {
+            if (HookArm_FixedUpdate.cloneArmPlacingObject)
+                return false;
+
+            if (__instance.transform.parent.GetInstanceID() != FistControl.Instance.transform.GetInstanceID())
+                return false;
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(Punch), "ForceHold")]
+    class Punch_ForceHold
+    {
+        static bool Prefix(Punch __instance)
+        {
+            if (HookArm_FixedUpdate.cloneArmPlacingObject)
+            {
+                //Debug.Log("Clone arm placing object");
+                return false;
+            }
+
+            if (__instance.transform.parent.GetInstanceID() != FistControl.Instance.transform.GetInstanceID())
+            {
+                //Debug.Log($"Parent mismatch: {__instance.transform.parent.GetInstanceID()} - {FistControl.Instance.transform.GetInstanceID()}");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ DualPunches/Plugin.cs
@@ -0,0 +1,192 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.AddressableAssets;
+using System.Linq;
+using UnityEngine.ResourceManagement.ResourceLocations;
+using UnityEngine.SceneManagement;
+
+namespace DualPunches
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ResourceLocationMap resourceMap = null;
+        public static T LoadObject<T>(string path)
+        {
+            if (resourceMap == null)
+            {
+                Addressables.InitializeAsync().WaitForCompletion();
+                resourceMap = Addressables.ResourceLocators.First() as ResourceLocationMap;
+            }
+
+            Debug.Log($"Loading {path}");
+            KeyValuePair<object, IList<IResourceLocation>> obj;
+            try
+            {
+                obj = resourceMap.Locations.Where(
+                    (KeyValuePair<object, IList<IResourceLocation>> pair) =>
+                    {
+                        return (pair.Key as string) == path;
+                        //return (pair.Key as string).Equals(path, StringComparison.OrdinalIgnoreCase);
+                    }).First();
+            }
+            catch (Exception) { return default(T); }
+
+            return Addressables.LoadAsset<T>(obj.Value.First()).WaitForCompletion();
+        }
+
+        public Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID + "_harmony");
+        public static GameObject blueArm;
+        public static GameObject redArm;
+        public static GameObject goldenArm;
+
+        static PropertyInfo hookArmInstance = typeof(MonoSingleton<HookArm>).GetProperty("Instance", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+
+        public static bool fastPunchEnabled = false;
+        private void OnSceneLoad(Scene before, Scene after)
+        {
+            //[HarmonyBefore(new string[] { "tempy.fastpunch" })]
+            fastPunchEnabled = Harmony.HasAnyPatches("tempy.fastpunch");
+
+            GameObject player = GameObject.Find("Player");
+            if (player == null)
+                return;
+            HookArm original = HookArm.Instance;
+            goldenArm = GameObject.Instantiate(player.transform.Find("Main Camera/Punch/Hook Arm").gameObject);
+            goldenArm.SetActive(false);
+            hookArmInstance.SetValue(null, original);
+        }
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            blueArm = LoadObject<GameObject>("Assets/Prefabs/Weapons/Arm Blue.prefab");
+            redArm = LoadObject<GameObject>("Assets/Prefabs/Weapons/Arm Red.prefab");
+            SceneManager.activeSceneChanged += OnSceneLoad;
+            harmony.PatchAll();
+
+
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+
+    /*
+    This was an attempt to make projectile boost stop intervals shorter 
+    */
+    /*[HarmonyPatch(typeof(TimeController), "ParryFlash")]
+    public class ParryFlash
+    {
+        static GameObject currentFlash;
+        static GameObject templateFlash;
+
+        static bool Prefix(TimeController __instance, ref GameObject ___parryLight)
+        {
+            if (templateFlash == null)
+            {
+                templateFlash = ___parryLight;
+                ___parryLight = new GameObject();
+            }
+            return true;
+        }
+
+        static void Postfix(TimeController __instance, GameObject ___parryLight)
+        {
+            if (currentFlash != null)
+                GameObject.Destroy(currentFlash);
+
+            currentFlash = GameObject.Instantiate<GameObject>(templateFlash, MonoSingleton<PlayerTracker>.Instance.GetTarget().position, Quaternion.identity, MonoSingleton<PlayerTracker>.Instance.GetTarget());
+            foreach(AudioSource src in currentFlash.GetComponents<AudioSource>())
+            {
+                src.priority = 1000000;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(TimeController), "TrueStop")]
+    public class TrueStopShortener
+    {
+        static float lastTime = 0;
+
+        static int requiredFreezes = 2;
+        static int freezeMulti = 1;
+
+        static bool Prefix(TimeController __instance, ref float __0)
+        {
+            if(lastTime > Time.time)
+            {
+                lastTime = Time.time;
+                return true;
+            }
+
+            float deltaTime = Time.time - lastTime;
+            lastTime = Time.time;
+            if(deltaTime > 1f)
+            {
+                requiredFreezes = 2;
+                freezeMulti = 1;
+            }
+            else
+            {
+                if(requiredFreezes == 0)
+                {
+                    freezeMulti += 1;
+                    requiredFreezes = 0;//1 << freezeMulti;
+                }
+                else
+                {
+                    requiredFreezes -= 1;
+                }
+            }
+
+            __0 /= freezeMulti;
+            return true;
+        }
+    }*/
+
+    [HarmonyPatch(typeof(DualWieldPickup), "PickedUp")]
+    public class DualWeaponPickup_Patch
+    {
+        static void Postfix(DualWieldPickup __instance)
+        {
+            Transform armContainer = FistControl.Instance.transform;
+            GameObject newPunch = new GameObject();
+            newPunch.transform.SetParent(armContainer, true);
+            newPunch.transform.localRotation = Quaternion.identity;
+
+            DualPunch[] componentsInChildren = armContainer.GetComponentsInChildren<DualPunch>();
+            if (componentsInChildren != null && componentsInChildren.Length % 2 == 0)
+            {
+                newPunch.transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else
+            {
+                newPunch.transform.localScale = Vector3.one;
+            }
+            if (componentsInChildren == null || componentsInChildren.Length == 0)
+            {
+                newPunch.transform.localPosition = Vector3.zero;
+            }
+            else if (componentsInChildren.Length % 2 == 0)
+            {
+                newPunch.transform.localPosition = new Vector3((float)((componentsInChildren.Length + 2) / 2) * -1.5f, 0f, 0f);
+            }
+            else
+            {
+                newPunch.transform.localPosition = new Vector3((float)((componentsInChildren.Length + 3) / 2) * 1.5f, 0f, 0f);
+            }
+
+            DualPunch dualWield = newPunch.AddComponent<DualPunch>();
+            dualWield.delay = 0.05f;
+            dualWield.juiceAmount = __instance.juiceAmount;
+            if (componentsInChildren != null && componentsInChildren.Length != 0)
+            {
+                dualWield.delay += (float)componentsInChildren.Length / 20f;
+            }
+        }
+    }
+}
